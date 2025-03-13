cff572d51b989e181f79d5bf404ad5d966c2eb29
tolonlo <tlondonol1@eafit.edu.co>
2025-03-12 19:12:04 -0500

    Eliminando carpetas innecesarias del repositorio

--- Proyecto_tomas/pagina1_simple/carpetas/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.lista_carpetas, name='lista_carpetas'),
-    path('crear/', views.crear_carpeta, name='crear_carpeta'),
-    path('carpeta/<str:nombre_carpeta>/', views.ver_carpeta, name='ver_carpeta'),
-]
--- Proyecto_tomas/pagina1_simple/pagina1_simple/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include  # Agrega include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('carpetas.urls')),  # Incluye las URLs de la app 'carpetas'
-]
