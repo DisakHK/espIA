844db25bc3a7d2725a09f99f427b7882e13a56ca
niosto <nicolasospinat@gmail.com>
2025-05-08 22:10:41 -0500

    add deployment variables

--- SinToFront/SinToFront/settings.py
+++ SinToFront/SinToFront/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-lb$!w3!%h1!f6y@gj%a_=1l4dg1t^fycx_orfju4xokz+7mt9h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
@@ -104,6 +104,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
